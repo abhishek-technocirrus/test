5a22a69eb90017077284ed99188b5b64126c21ec
abhishek-technocirrus <122975784+abhishek-technocirrus@users.noreply.github.com>
2023-06-20 13:34:46 +0530

    Add files via upload

--- code/update.py
+++ code/update.py
@@ -1,15 +1,15 @@
 from pymongo import MongoClient
 import os
 # Retrieve the host and port from environment variables
-db_host = os.environ.get('DB_HOST')
-db_port = int(os.environ.get('DB_PORT'))
+db_host = docdb-2023-06-20-07-52-242.cgognpdsxe7j.us-east-1.docdb.amazonaws.com
+db_port = 27017
 
 # Connect to the DocumentDB cluster
 client = MongoClient(db_host, db_port)
 
 # Authenticate with the provided credentials
 db = client.my_database
-db.authenticate('username', 'somepass123')
+db.authenticate('abhishek', 'Temp1234')
 
 # Update a document in a collection
 collection = db.my_collection
